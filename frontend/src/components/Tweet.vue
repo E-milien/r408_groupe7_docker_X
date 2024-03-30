47fc8a90e9b31ed71161f0d7ec4debb5aaa02eb1
e-milien <emilien.roukine@gmail.com>
2024-03-30 21:40:41 +0100

    envoie creation page d'acceuil

---
+++ frontend/src/components/Tweet.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const props = defineProps({
+    tweet: {
+        required: true,
+        type: Object
+    }
+});
+
+const liked = ref(false);
+
+const toggleLike = () => {
+    liked.value = !liked.value;
+};
+</script>
+
+<template>
+    <div>
+        <h3>{{ tweet.user_id }}</h3>
+        <p class="text">{{ tweet.tweet_text }}</p>
+        <p class="like">
+            <button @click="toggleLike">{{ liked ? 'UnLike' : 'Like' }}</button>
+        </p>
+    </div>
+</template>
+
+<style scoped>
+div {
+    margin-bottom: 20px;
+    padding: 20px;
+    background-color: #f7f7f7;
+    border-radius: 5px;
+}
+
+h3 {
+    margin: 0;
+    color: #333;
+}
+
+.text {
+    color: #666;
+}
+
+.like {
+    margin-top: 10px;
+}
+
+.like button {
+    padding: 8px 20px;
+    background-color: #1da1f2;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.like button:hover {
+    background-color: #0b7bc6;
+}
+</style>
--- frontend/src/store/Tweet.js
+++ frontend/src/store/Tweet.js
@@ -3,11 +3,11 @@ import { defineStore } from 'pinia'
 
 export const useTweetStore = defineStore('tweet', () => {
     const allTweet = ref(null)
-    async function getTweet() {
+    async function getTweets() {
         const response = await fetch('http://localhost:5020/api/tweet', { method: 'GET' })
 
         allTweet.value = await response.json()
         return response
     }
-    return { getTweet, allTweet }
+    return { getTweets, allTweet }
 })

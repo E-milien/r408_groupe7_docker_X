2234b222f0f63deaaae876daa2b0fdeac29de7f2
e-milien <emilien.roukine@gmail.com>
2024-04-01 14:48:41 +0200

    validation

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\TweetController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LikeController;
